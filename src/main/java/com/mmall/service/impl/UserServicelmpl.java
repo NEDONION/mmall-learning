9e9c17636089255189e809474c527b5ab0d289ff
NEDONION <nedjiachenghu@gmail.com>
2022-01-28 21:27:31 -0600

    feature: user responsecode

---
+++ src/main/java/com/mmall/service/impl/UserServicelmpl.java
@@ -0,0 +1,11 @@
+package com.mmall.service.impl;
+
+import com.mmall.service.IUserService;
+
+public class UserServicelmpl  implements IUserService {
+
+    @Override
+    public Object login(String username, String password) {
+        return null;
+    }
+}
